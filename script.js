d332a989590dcee97c7bcb7e03c2e3f09f4bab69
Agnes Chueng <agneschueng@gmail.com>
2020-10-06 23:58:27 -0500

    added api url and key

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Javascript will start running once HTML is done loading
 $(document).ready(function(){
 
-
-
+// Cities that will be taken from User Input -- Search
+    var cityName = "";
+// Open Weather API URL
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=53b6f538d72c7744df9aec607b80628c";
+    
 
 
 
