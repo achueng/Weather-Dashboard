ad46e65514d32713f6ce2bf8c321f7d4f2e6d21b
Agnes Chueng <agneschueng@gmail.com>
2020-10-07 18:02:10 -0500

    added click event on search button

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ $(document).ready(function(){
     // userInput from search after clicking the search button will be cityName
     var userInput; // will be defined in click event
 
+    // PSEUDO CODE
+        // When user clicks search button
+        $("#search-button").on("click", () => {
+            // 1. take value of input search and assign it to userInput
+            userInput = $("#search").val().trim().toLowerCase();
+            console.log(userInput);
+                // if value is "" don't do anything
+                // else run the following
+            // 2. assign userInput to cityName
+        });
+        
     // Open Weather API URL
     var queryURL = "https://cors-anywhere.herokuapp.com/"+ "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=53b6f538d72c7744df9aec607b80628c";
 
@@ -16,21 +27,14 @@ $(document).ready(function(){
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        // console.log(response);
 
-        /* PSEUDO CODE
-        When user clicks search button
-            1. take value of input search and assign it to userInput
-                if value is null don't do anything
-                else run the following
-            2. change userInput to all lowercase and assign to cityName
-        
-        Take the following:
-            1. temp -> response.main.temp
-            2. humidity -> response.main.humidity
-            3. wind -> response.wind.speed
-        Use .text(the above) to its respectable class
-        */
+        // PSEUDO CODE
+        // Take the following:
+        //     1. temp -> response.main.temp
+        //     2. humidity -> response.main.humidity
+        //     3. wind -> response.wind.speed
+        // Use .text(the above) to its respectable class
 
         
         
