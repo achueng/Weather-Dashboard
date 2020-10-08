a01cef7cc31fa6772c476ca43f0dfd27547bfa63
Agnes Chueng <agneschueng@gmail.com>
2020-10-08 02:17:34 -0500

    add api url for uv index

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
 //Declaring global variables
     // Cities that will be taken from User Input -- Search
     var cityName; // will be assigned the value of userInput
-
-    // PSEUDO CODE
+  
     // When user clicks search button
     $("#search-button").on("click", () => {
         // 1. take value of input search and assign it to userInput
@@ -18,7 +17,8 @@ $(document).ready(function(){
         else {
             cityName = userInput;
         }
-        // Open Weather API URL
+
+        // Open Weather API URL for general weather data
         var genURL = "https://cors-anywhere.herokuapp.com/"+ "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=53b6f538d72c7744df9aec607b80628c";
     
         // AJAX call for current city weather
@@ -26,11 +26,7 @@ $(document).ready(function(){
             url: genURL,
             method: "GET"
         }).then(function(response){
-            // PSEUDO CODE
-            // Take the following:
-            //     1. temp -> response.main.temp
-            //     2. humidity -> response.main.humidity
-            //     3. wind -> response.wind.speed
+            // Take the following the temp, humid, wind from response
             // Use .text(the above) to its respectable class
             var name = $(".city-name").text(response.name);
             var temp = $(".temp").text("Temperature: " + response.main.temp.toFixed(1) + "\xB0F");
@@ -38,13 +34,23 @@ $(document).ready(function(){
             var wind = $(".wind").text("Wind Speed: " + response.wind.speed.toFixed(1) + " MPH");
             $(".overview").append(name, temp, humid, wind);
 
-            // PSEUDO CODE
+            // Grab latitude & longitude of city, use in uv ajax call
+            var lat = JSON.stringify(response.coord.lat);
+            var lon = JSON.stringify(response.coord.lon);
+
             // Create a button for each city after user searches
             var newCity = $("<button>").text(response.name);
             newCity.addClass("clear button warning city-button"); // use ".city-button" for click event later
             newCity.attr("data-city", cityName); // use data-city later
             // append button to nav
             $(".cities").append(newCity);
+        
+
+            // Open Weather API URL for UV index
+            var uvURL = "https://cors-anywhere.herokuapp.com/"+ "http://api.openweathermap.org/data/2.5/uvi?lat="+ lat +"&lon="+ lon +"&appid=53b6f538d72c7744df9aec607b80628c";
+
+            // console.log(uvURL);
+
             // After refreshing the page, the user will still have access to the city's weather data -- localStorage
                 // each localstorage setItem will have a unique name
             // clicking the button will get the user the city's weather info
